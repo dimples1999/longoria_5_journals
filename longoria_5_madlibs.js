7c7df68a6c4e21fbe05801687cfb7f56966c3410
dimples1999 <glongoria@ninja10.d22.lan>
2016-02-05 14:12:01 -0600

    my madlibs

---
+++ longoria_5_madlibs.js
@@ -0,0 +1,38 @@
+var name = "Melodie";
+
+var name2 = "Delilah";
+
+var hours= "7";
+
+var vehicle= "limo";
+
+var place= "Magic Mountain";
+
+var noun1="street";
+
+var verb1="dancing";
+
+var verb2="fascinated";
+
+var verb3="rescued";
+
+var adj1="beautiful";
+
+var adj2="funny";
+
+var adj3="inexpensive";
+
+var prep1=" around";
+
+
+document.write("Last month, " +name +" went to Disney " + "World with " +name2 +".");
+document.write(" They traveled for " +hours +" hours by " +vehicle +".")
+document.write(" Finally they got there and it was very " +adj1 +", ")
+document.write("and there were " +adj2 +" characters " +verb1 +prep1 +".")
+document.write(" People laughing everywhere.")
+document.write(" I wish it would have been more " +adj3 +",")
+document.write(" but we were " +verb2 +" anyways.")
+document.write(" We also went on some " + " crazy ride, " +" called magic " +noun1 +". ")
+document.write(name +" nearly fell off a ride and had to be " +verb3 +" by Mickey Mouse!")
+document.write(" Later they went to the hotel and had a sleepover.")
+document.write(" Next year, " +name2 +" wants to go to " +place +"," +" where they can have some more fun.")
