f6f99b476fe6be59b551f7554f0330141072c541
dimples1999 <glongoria@ninja10.d22.lan>
2016-02-26 13:11:10 -0600

    Fixed the problems with my quiz game

---
+++ longoria_5_quizgame.js
@@ -0,0 +1,81 @@
+var name = prompt("whats your name?");
+
+var one = prompt("What is Gabriela's last name?");
+
+if (one == "longoria") {
+    alert("wow you got it right!");
+} else { 
+    alert("are you serious? wow just wow :(")
+}
+
+var two = prompt("Which 2 singers are her favorite?");
+
+if (two == "michael jackson and prince" ) {
+    alert("its not that impressive kinda basic knowledge but whatever. ");
+} else { 
+    alert("not even close...")
+}
+
+ var three = prompt("What is her favorite color?");
+
+if (three == "red" ) {
+    alert("hey thats pretty good!");
+} else { 
+    alert("not at all :(")
+}
+
+  var four = prompt("what does she hate?");
+
+if (four == "drama" ) {
+    alert("nailed it my dude!");
+} else { 
+    alert("ummm.. nope")
+}
+
+  var five = prompt("what month was she born?");
+
+if (five == "october"  ) {
+    alert("der ya go. ");
+} else { 
+    alert("wrooonngg!!!")
+}
+
+  var six = prompt("what is her favorite food?");
+
+if (six == "pizza"  ) {
+    alert("piiizzzaa!!! ");
+} else { 
+    alert("are you even trying?")
+}
+
+  var seven = prompt("whos her inspiration to become a fashion designer?");
+
+if (seven == "selena quintanilla"  ) {
+    alert("Yaaasss!! ");
+} else { 
+    alert("That's just sad.. try again!")
+}
+
+  var eight = prompt("who is her best friend?");
+
+if (eight == "taj"  ) {
+    alert("You got it! ");
+} else { 
+    alert("woooww... this is disappointing..")
+}
+
+  var nine = prompt("whats her favorite place to eat?");
+
+if (nine == "johnny rockets"  ) {
+    alert("correct! ");
+} else { 
+    alert("incorrect :(")
+}
+
+  var ten = prompt("what's her favorite drink?");
+
+if (ten == "orange soda"  ) {
+    alert(" all done good job dude!");
+} else { 
+    alert("noooo!!!")
+}
