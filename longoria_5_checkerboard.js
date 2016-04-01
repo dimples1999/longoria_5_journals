bb0eeddb7aae9ef68b13cdb51da044d86537bcf8
dimples1999 <glongoria@ninja10.d22.lan>
2016-04-01 13:12:52 -0500

    my checkerboard

---
+++ longoria_5_checkerboard.js
@@ -0,0 +1,25 @@
+#chessboard, .black_square, .white_square {
+	display:block;
+	padding:0px;
+	margin:0px;
+}
+
+#chessboard {
+	width:1000px;
+	height:1000px;
+}
+
+.black_square, .white_square {
+	width:120px;
+	height:120px;
+	float:left;
+}
+
+.black_square {
+	background-color:#6FCDAC;
+}
+
+.white_square {
+	background-color:#B173CC;
+}
+
